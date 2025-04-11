7da9225b5d27508ffe4a14a426abb4136fa3a0f7
howardliao0211 <howard.liao.hh@gmail.com>
2025-04-12 01:00:58 +1000

    fix: updated inference code.

--- inference.py
+++ inference.py
@@ -4,6 +4,7 @@ from PIL import Image
 from torchvision import transforms
 from Util import show_triplet_img
 from Model import *
+from Data import *
 
 def load_image(image_path: str) -> torch.Tensor:
     image = Image.open(image_path).convert("RGB")
@@ -28,18 +29,20 @@ def postprocess_embedding(embeddging1: torch.Tensor, embedding2: torch.Tensor) -
 
 if __name__ == "__main__":
     model_path = r"Trained_Models\Baseline_FaceNet.pkl"
-    anchor_path = r"Data\lfw_224\Adam_Sandler\Adam_Sandler_0001.jpg"
-    positive_path = r"Data\lfw_224\Adam_Sandler\Adam_Sandler_0003.jpg"
-    negative_path = r"Data\lfw_224\Abdel_Madi_Shabneh\Abdel_Madi_Shabneh_0001.jpg"
+    LFW_DIR = r'./Data/lfw_224.zip'
+    _, test_loader = get_dataloader(LFW_DIR, transform=None, val_percent=10, batch_size=64)
 
     model = FaceNet()
-    checkpoint = torch.load(r'Checkpoints\FaceNet_ResNeXt_20250410_170040_epoch16.pt', map_location='cpu')
+    checkpoint = torch.load(r'Checkpoints\20250411_115256\FaceNet_ResNeXt_epoch20.pt', map_location='cpu')
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
 
-    anchor_image = load_image(anchor_path)
-    positive_image = load_image(positive_path)
-    negative_image = load_image(negative_path)
+    data_index = 77
+    dataset = test_loader.dataset
+
+    anchor_image = dataset[data_index][0]
+    positive_image = dataset[data_index][1]
+    negative_image = dataset[data_index][2]
 
     distance_threshold = 1.1
 
@@ -50,10 +53,7 @@ if __name__ == "__main__":
 
         anchor_to_positive_distance = postprocess_embedding(anchor_embedding, positive_embedding)
         anchor_to_negative_distance = postprocess_embedding(anchor_embedding, negative_embedding)
-    
-        print(f"Distance between anchor and positive: {anchor_to_positive_distance}")
-        print(f"Distance between anchor and negative: {anchor_to_negative_distance}")
-    
+
+    print(f"Distance between anchor and positive: {anchor_to_positive_distance}")
+    print(f"Distance between anchor and negative: {anchor_to_negative_distance}")
     show_triplet_img([(anchor_image, positive_image, negative_image)])
-
-
